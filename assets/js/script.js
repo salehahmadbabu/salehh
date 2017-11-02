4e077af270d5f6da21969025efbee912018ef85f
salehdevs <salehdevs@gmail.com>
2017-11-02 12:26:03 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+$(function(){
+	$(".home-slider").owlCarousel({
+		items: 1,
+		loop: true,
+		dots: false,
+		autoplay: true,
+		autoplayHoverPause: true,
+		smartSpeed: 2000,
+	});
+
+
+	$(".review-list").owlCarousel({
+		items: 1,
+		loop: true,
+		dots: false,
+		autoplay: true,
+		autoplayHoverPause: true,
+		smartSpeed: 700
+	});
+
+	$('.grid').isotope({
+  // options
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows'
+});
+	
+})
